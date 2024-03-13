fdd25916449a325a22668f5c4026e6d695f81782
developerXDhanjit <160407989+developerXDhanjit@users.noreply.github.com>
2024-03-13 07:11:01 +0000

    added datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,8 +4,29 @@
 
 console.log(2+2)
 
+
+/* Datatypes -> Primitive ->
+string , number , boolean , bigint , null , undefined , symbol  */
+
+let userName = "Dhanjit Nath " 
+let userAge = 17
+let isLoggedIn = false
+
+console.log(typeof userName , userName)
+console.log(typeof userAge , userAge)
+console.log(typeof isLoggedIn , isLoggedIn)
+
+console.log(typeof null , null)
+console.log(typeof undefined , undefined);
+
+
 /* Note : -
 1. Code should be readable so give spacing only as needed. 
-
-
+2. Null is also a standalone value -> Empty 
+3. Undefined means the value isnt assigned yet . 
+4. Symbol is mostly  used to define something unique 
+5. typeof is used to know the type of the data 
 */
+
+
+
