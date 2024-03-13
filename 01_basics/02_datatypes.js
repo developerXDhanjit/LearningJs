fa9d73abb645256a5ada431846266bcfd6af753b
developerXDhanjit <160407989+developerXDhanjit@users.noreply.github.com>
2024-03-13 06:57:46 +0000

    datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // Treats all the js as per the newer version
+
+// alert("hellow!") // not in nodejs 
+
+console.log(2+2)
+
+/* Note : -
+1. Code should be readable so give spacing only as needed. 
+
+
+*/
