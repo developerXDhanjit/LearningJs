d8033ef2cba749f3f7ffbbd22d9606c94e1ef7d0
developerXDhanjit <160407989+developerXDhanjit@users.noreply.github.com>
2024-03-14 08:20:44 +0000

    Datatypes file 2  added

---
+++ 01_basics/05_datatypes2.js
@@ -0,0 +1,30 @@
+//Non Primitive  Datatypes
+// Arrays , objects , functions
+
+const score = 100;
+const scoreExact = 100.8;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+let bigNumber = 334993849384498339393303n;
+
+let isLoggedIn = true;
+let temp = null;
+let userEmail;
+
+/* 
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId);
+console.log(bigNumber);
+console.log(typeof bigNumber); */
+
+
+//Arrays 
+
+/* const anime = ["Your Name " , " I want to eat your pancreas" , "A Silent voice " , "5 cm per second"]
+console.log(anime
+
+/* Note : 
+1. Dynamically typed language vs static --> defining the type of the variable */
