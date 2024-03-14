3f03dcf5d20d8d869eac252996639ffc5c667a4d
developerXDhanjit <160407989+developerXDhanjit@users.noreply.github.com>
2024-03-14 07:45:04 +0000

    Comparsion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+/* Simple Comparision Operators  */
+/*
+//EASY and pretictable 
+console.log( 2 > 3 ); //Greater than 
+console.log( 2 < 3 ); //less than 
+console.log( 2 == 3 ); //equal to   
+console.log( 2 != 3 ); //Greater than 
+console.log( 2 >= 3 ); //Not greater than 
+console.log( 2 <= 3 ); //not less than  */
+
+/* Comparision between different datatypes  
+=> Not recommended as it is not pretictable , results sometimes may vary 
+As you can see below in case of null 
+*/
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+/* console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); */ //Prints true whooh -> Reason operators works differently
+
+
+// === => Checks the value strictly 
+
+console.log("2" === 2); //Prints false since datatype is not same
