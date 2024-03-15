716842143135388aa2d780b4542aff8a1f6e8fb0
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-15 09:29:01 +0530

    Added split

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -8,7 +8,7 @@ let friends = 100;
 console.log(`${userName} has ${friends} friends`); // String Interpolation
 
 const newName = new String("Hitesh Choudhary ");
-
+/* 
 console.log(newName);
 
 console.log(newName.__proto__); // An object with the string properties
@@ -16,10 +16,10 @@ console.log(newName.__proto__); // An object with the string properties
 console.log(newName.length);
 console.log(newName.toUpperCase()); //upper case
 
-console.log(newName); // Doesnt change the original string
+console.log(newName); */ // Doesnt change the original string
 
-console.log(newName.charAt(2));
-console.log(newName.indexOf("t"));
+// console.log(newName.charAt(2));
+// console.log(newName.indexOf("t"));
 
 //Additional methods
 
@@ -27,21 +27,21 @@ let newString = newName.substring(0, 4);
 // 4 is excluded
 //Cant give neg value in substring
 
-console.log(newString);
+// console.log(newString);
 
 let anotherString = newName.slice(0, -4);
 //Interesting --> negative means starts from the end
 
-console.log(anotherString);
+// console.log(anotherString);
 
+let otherString = "dhanjit nath is a programmer";
+// console.log(otherString);
+// console.log(otherString.trim()); //Removes the spaces in the start and end of the string
 
-let otherString = "    dhanjit  nath    "
-console.log(otherString);
-console.log(otherString.trim()); //Removes the spaces in the start and end of the string
+let url = "http://example.com/test%20test.html";
+// console.log(url);
+// console.log(url.replace("%20", "_"));
 
+//console.log(url.includes("test"));
 
-let url = "http://example.com/test%20test.html"
-console.log(url);
-console.log(url.replace("%20", "_"));
-
-console.log(url.includes('test'));
+console.log(otherString.split(" "));
