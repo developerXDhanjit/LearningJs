e392ee684201b59e7e79de955e826da114dcbb44
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-16 12:30:55 +0530

    Destructuring in JS

---
+++ 02_basics/05_destructuring.js
@@ -0,0 +1,16 @@
+let thisCourse = {
+    courseName : "js-hindi youtube ",
+    price : 0,
+    courseInstructor : "Hitesh Sir"
+}
+
+const {courseName} = thisCourse 
+const {courseName : cName} = thisCourse 
+
+// console.log(courseName);
+// console.log(cName);
+// console.log(thisCourse.courseName);
+//No errors all are doing the same job
+console.log(thisCourse);
+
+/*  */
