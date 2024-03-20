85b0f4625e16538866d32637905ea58a5cb8904a
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-20 13:43:51 +0530

    Switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,55 @@
+/*
+Syntax 
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
+let monthNumber = 8;
+
+switch (monthNumber) {
+    case 1:
+        console.log("JANUARY");
+        break;
+    case 2:
+        console.log("FEBRAURY");
+        break;
+    case 3:
+        console.log("MARCH");
+        break;
+    case 4:
+        console.log("APRIL");
+        break;
+    case 5:
+        console.log("MAY");
+        break;
+    case 6:
+        console.log("JUNE");
+        break;
+    case 7:
+        console.log("JULY");
+        break;
+    case 8:
+        console.log("AUGUST");
+        break;
+    case 9:
+        console.log("SEPTEMBER");
+        break;
+    case 10:
+        console.log("OCTOBER");
+        break;
+    case 11:
+        console.log("NOVEMBER");
+        break;
+    case 12:
+        console.log("DECEMBER");
+        break;
+
+    default:
+        console.log("Default Value");
+        break;
+}
