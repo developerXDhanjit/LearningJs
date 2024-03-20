c2d2c871317f1eb8996ca21bb98d417e79a70ec6
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-20 15:51:42 +0530

    Terniary operator added

---
+++ 04_control_flow/05_terniary.js
@@ -0,0 +1,10 @@
+/* Terniary operator , is different from NC  */
+
+// condition ? true : false
+
+let val1 = 8;
+// console.log(val1 == 5 ? 5 : "NO value");
+/* val1 > 7
+  ? console.log("Value is greater than 7")
+  : console.log("Value is less than 7"); */
+/* Both are the ways to write it  */
