bf87e04989d38ee1f248a46064fb67eb33a26325
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-20 15:36:31 +0530

    Nullish coalescing operator

---
+++ 04_control_flow/04_nc_operator.js
@@ -0,0 +1,10 @@
+/* Nullish Coalescing Operator ?? */
+
+let val1 ;
+// val1 = 5 ?? 10 // value is 5 
+// val1 = null ?? 10 // value is 10
+// val1 = undefined ?? 15 //same logic => 15
+console.log(val1);
+
+/* It is used incase the value is null , we have and option to change it and use something else to save our program
+Basically its a callback to assign the value as we choose incase there is something ... like if-else a bit easiler */
