41af6ed9e61b9009d7605f47ea7de3e92dcb974b
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-21 14:13:45 +0530

    Break and continue statements

--- 05_iterations.js/01_for.js
+++ 05_iterations.js/01_for.js
@@ -20,8 +20,37 @@ for (let i = 1; i < 11; i++) {
 /* Using this logic to create multiplication table  */
 
 for (let outer = 1; outer < 11 ; outer++) {
-  console.log(` Table for ${outer} `);
+  //console.log(` Table for ${outer} `);
   for (let j = 1; j < 11 ; j++) {
-    console.log(` ${outer} * ${j} = ${outer*j} `);   
+   // console.log(` ${outer} * ${j} = ${outer*j} `);   
   }  
+}
+
+/* Looping in arrays  */
+let myArr = ["Honesty", "Brave" , "Happiness"]
+
+for (let index = 0; index < myArr.length; index++) {
+  const element = myArr[index];
+ // console.log(element); 
+}
+
+
+//Break and continue 
+
+for (let index = 0; index < 20; index++) {
+if (index == 7) {
+  console.log(`The number ${index} detected`);
+  break;
+}
+console.log(`Value of Index is : ${index}`);
+}
+
+for (let index = 0; index < 20; index++) {
+if (index == 7) {
+  console.log(`The number ${index} detected`);
+  continue;
+  //Forgive for this condition 
+  // Doesnt get out of the loop 
+}
+console.log(`Value of Index is : ${index}`);
 }
