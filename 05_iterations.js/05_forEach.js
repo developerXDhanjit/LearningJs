a3641c48de846b7cf84ff0b975576d5543a18563
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-22 14:52:47 +0530

    For Each added

---
+++ 05_iterations.js/05_forEach.js
@@ -0,0 +1,27 @@
+let languages = ["Java", "Javascript", "Html", "Python", "C++"];
+
+languages.forEach(function (item) {
+  // console.log(item);
+});
+
+languages.forEach((item) => {
+  // console.log(item);
+});
+
+/* Since its a callback function we are not to write the names of the fns , here */
+
+/* Here is something cool  */
+
+const PrintMe = (item) => {
+//   console.log(item);
+};
+
+languages.forEach(PrintMe);
+
+/* Remember to only give the reference of the function here and not execute it  */
+
+/* Parameter in this array are not just item , it conisits of item , index and the whole array */
+
+languages.forEach((item, index, arr) => {
+  console.log(item , index, arr);
+});
