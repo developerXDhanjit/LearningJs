138d3bcf81bfea25ff8dfcaf1592ff2ae8535c8f
developerXDhanjit <developerdhanjit@gmail.com>
2024-03-23 18:44:13 +0530

    Reduce Method

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,11 +1,44 @@
 const nums = [1, 2, 3, 4];
 let initialVal = 0;
-
+/* 
 let reduceNums = nums.reduce(function (acc,curv) {
+  console.log(`acc : ${acc} , curv : ${curv}`);
   return acc + curv;
-}, 0)
+}, 100) */
+
+//Using arrowfunction 
+let reduceNums = nums.reduce((acc,curv)=> (acc+curv),initialVal)
 
 /* const reducedNums = nums.reduce((accumulator, currentValue) => {
   return accumulator + currentValue, initialVal;
 }); */
-console.log(reduceNums);
+// console.log(reduceNums);
+
+let shoppingCart = [
+  {
+    item : 'Mango',
+    price : 149
+  },
+  {
+    item : 'Apple',
+    price : 59
+  },
+  {
+    item : 'Grapes',
+    price : 69
+  },
+  {
+    item : 'Bread',
+    price : 10
+  },
+  {
+    item : 'Uncle Chips',
+    price : 10
+  },
+]
+
+/* To print the total price of the cart  */
+
+let totalPrice = shoppingCart.reduce((acc,item)=> acc + item.price ,0)
+
+console.log(`Total Price of the Cart is : ${totalPrice}`);
