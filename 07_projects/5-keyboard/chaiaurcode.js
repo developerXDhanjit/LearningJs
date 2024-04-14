48dea92bf7cdbd28c68d816feffa242273534648
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-14 11:49:00 +0530

    Project 5  Added

--- 07_projects/5-keyboard/chaiaurcode.js
+++ 07_projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,27 @@
-console.log('Project 5');
+let insert = document.querySelector('#insert');
+
+window.addEventListener('keydown', function (e) {
+
+    insert.innerHTML = `
+    
+    <div id = 'color' > 
+
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>KeyCode</th>
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? "space" : e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+  </table> 
+    </div>  
+    
+    `
+});
+
+
+
