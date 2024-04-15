59d4cf647fa5116584902e6d437505776c763ef9
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-15 22:50:41 +0530

    Some thoughts on OOPs

---
+++ 11_oops/01_oop.js
@@ -0,0 +1,35 @@
+/* Object literal means literally an object  */
+
+const methods = {
+    name: "DeveloperXDhanjit",
+    coding: "Always",
+    javascript: "forever",
+
+    sayHello: function () {
+        return 'Hellow'
+    },
+
+    addTwoNum: function (a, b) {
+        return a + b
+    },
+
+    greetWell : function () {
+        // console.log(this); //Print the current object methods
+        return ` Welcome ${this.name} , You are ${this.coding} Coding `
+
+        
+        //this keyword is used to show the current context here
+    }
+
+
+}
+
+console.log(methods.sayHello());
+console.log(methods.addTwoNum(2, 4));
+console.log(methods.greetWell());
+
+/* Use of this keyword , for reference of which data , arr , obj  
+
+Will be more detailed in prototypes
+*/
+
