ab1db61756a251ccfe3adccfa43cb813f8fe2a4c
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-15 22:09:15 +0530

    Some theory part on working of fetch

---
+++ 10_apiHandling/03_fetch.js
@@ -0,0 +1,21 @@
+/* How useful fetch is in comparision to the old method */
+
+/* MicroTask Que === Priority Que 
+
+even if same time is taken for two async functions 
+Fetch is shown before others 
+
+Working Mechanism of fetch
+
+(Fetch) => Data && Browser Api 
+
+Data --> Memory allocation ,, Arrays , , readonly
+-> Onfullfiling  || OnRejection 
+
+Browser/Node --> Network Req
+--> Yes or No 
+--> if yes the it goes to OnFullfilling
+--> if No it goes on OnRejection
+
+
+*/
