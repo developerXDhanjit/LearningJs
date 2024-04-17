c447a2a6d895a898502a20f0924c1394ae7404d3
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-17 17:14:11 +0530

    Added more about oops in JS

--- 11_oops/01_oop.js
+++ 11_oops/01_oop.js
@@ -13,11 +13,11 @@ const methods = {
         return a + b
     },
 
-    greetWell : function () {
+    greetWell: function () {
         // console.log(this); //Print the current object methods
         return ` Welcome ${this.name} , You are ${this.coding} Coding `
 
-        
+
         //this keyword is used to show the current context here
     }
 
@@ -40,4 +40,50 @@ Will be more detailed in prototypes
 
 //new is a constructor func 
 //Multiple instances from the same object literal 
+
+
+/* Lets try something with functions like objects ... */
+
+function user(username, isLoggedIn, id) {
+    this.username = username;
+    this.isLoggedIn = isLoggedIn;
+    this.id = id;
+
+    return this
+
+    // return {username,isLoggedIn,id}
+    //use this instead 
+}
+
+/* let user1 = user("Dhanjit", "true", 8989)
+let user2 = user("Hitesh", "false", 8990)
+ */
+// console.log(user1); //Value gets overitten 
+// console.log(user2);
+
+
+/* To create a new instance we use the keyword new  */
+
+let user1 = new user("Dhanjit", "true", 8989)
+let user2 = new user("Hitesh", "false", 8990)
+
+/* console.log(user1);
+console.log(user2); */
+
+/* Notes for new keyword 
+
+1. An empty object is created at first 
+2. A constructor function is called because of the new keyword
+   Argument are packed in it and given 
+3. The argument are injected in it . {this}
+4. We get it inside the function 
+
+
+*/
+
+
+/* Constructor  */
+
+console.log((user1.constructor)()); // Returns a function user  , i invoked it and checked the values 
+ 
 
