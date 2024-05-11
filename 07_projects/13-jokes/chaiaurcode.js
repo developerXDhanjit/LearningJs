b4e2e2143c93a6c6e9c50696c0585f737e4a0a7a
developerXDhanjit <developerdhanjit@gmail.com>
2024-05-11 21:41:48 +0530

    added project 13 using race method

--- 07_projects/13-jokes/chaiaurcode.js
+++ 07_projects/13-jokes/chaiaurcode.js
@@ -1,7 +1,21 @@
 const url = 'https://api.chucknorris.io/jokes/random';
+const displayJoke = document.querySelector("#display-joke")
+const getJoke = document.querySelector("#getJoke")
 
 // handle this end point with XMLHttpRequest
 
 // handle this end point with promises
 
 // handle the case of race condition
+
+
+async function fetchRandomJoke(){
+    let respone = await fetch(url)
+    let data = await respone.json()
+    displayJoke.textContent = data.value
+}
+
+getJoke.addEventListener("click" , (event)=>{
+    fetchRandomJoke()
+})
+
