b670a21050ab7a146a56ccc531899ca9f21eff89
developerXDhanjit <developerdhanjit@gmail.com>
2024-05-11 21:29:00 +0530

    project 12 added

--- 07_projects/12-randomImage/chaiaurcode.js
+++ 07_projects/12-randomImage/chaiaurcode.js
@@ -1,3 +1,32 @@
 const baseURL = 'https://source.unsplash.com/all/300x300';
 
-// this url gives an image. Use this and NO API calls
+/* Steps : 
+  1. get the button , content in js 
+  2. get the img src 
+  3. append the img in content 
+
+  but , i saw the same image reapeating the changed the query and now , with math .random , changed quite a few things 
+
+ */
+
+
+const content = document.querySelector(".content")
+const button = document.querySelector('#getImage')
+
+
+
+
+function imagefeed() {
+    let random = Math.floor(Math.random() * 100)
+    const img = document.createElement("img");
+    img.src = `${baseURL}?${random}`
+    content.appendChild(img)
+
+    random = Math.floor(Math.random() * 100)
+    console.log(img.src);
+}
+
+
+button.addEventListener("click", (event) => {
+    imagefeed()
+})
